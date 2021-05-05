a0aa7186c3a30ad4fa08291339c78282e2af2861
Coding-Tams <82816924+Coding-Tams@users.noreply.github.com>
2021-05-05 09:23:45 +0100

    Update main.js

---
+++ module-3/task/start/scripts/main.js
@@ -0,0 +1,9 @@
+//this is a single line comment
+
+const productName = 'Baked beans';
+let price = 0.89;
+let quantity = 2;
+let inStock = true;
+let discountAmount = 0.20;
+
+console.log( productName, price );
