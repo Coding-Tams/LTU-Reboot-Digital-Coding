92a8ec4331ae89ec556c13a619120ac9c4999b16
Coding-Tams <82816924+Coding-Tams@users.noreply.github.com>
2021-05-05 11:18:05 +0100

    Update main.js
    Create function and converted to arrow function.
    Did if and else statements.
    Basic arithmetic

--- module-3/task/start/scripts/main.js
+++ module-3/task/start/scripts/main.js
@@ -6,4 +6,30 @@ let quantity = 2;
 let inStock = true;
 let discountAmount = 0.20;
 
-console.log( productName, price );
+//console.log( productName, price );
+
+function showProductName() {
+    console.log (productName);
+}
+showProductName();
+
+function totalPrice(productPrice,productQuantity) {
+    console.log(productPrice * productQuantity) ;
+
+}
+//total(PriceQuantity)
+
+let checkAvailability = productInStock => console.log(productInStock);
+
+checkAvailability (inStock);
+
+function productDiscount() {
+    if(quantity > 1) {
+      let newPrice = (price - discountAmount * quantity);
+      console.log(newPrice);
+     } else{
+         console.log( price * quantity);
+     }
+}
+
+productDiscount();
