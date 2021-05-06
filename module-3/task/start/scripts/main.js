db4a8bafcabece6c480586ed7ef21f045a6c9ebb
Coding-Tams <82816924+Coding-Tams@users.noreply.github.com>
2021-05-06 11:23:44 +0100

    Update main.js
    I

--- module-3/task/start/scripts/main.js
+++ module-3/task/start/scripts/main.js
@@ -33,3 +33,60 @@ function productDiscount() {
 }
 
 productDiscount();
+
+/*
+check drink
+coke
+lemonade
+orangeade
+
+*/
+function drinkOrder (drink, size) {
+    switch(drink){
+        case 'coke':
+         console.log(drink);
+         break;
+        case 'lemonade':
+        console.log(drink);
+        break;
+        case 'orangeade':
+            console.log(drink);
+            break;
+            default:
+            console.log(drink, 'not available')
+            break;
+    }
+    drinkOrder('coke', 'extra large');
+
+}
+
+
+function calculator (num1, num2, operator) {
+    switch(operator){
+        case '+':
+        console.log(`${num1} ${operator} ${num2} = ${num1 + num2}`);
+        break;
+        case '-':
+            console.log(`${num1} ${operator} ${num2} = ${num1 - num2}`);
+        break;
+        case  '*':
+            console.log(`${num1} ${operator} ${num2} = ${num1 * num2}`)
+        break;
+        case '/':
+            console.log(`${num1} ${operator} ${num2} = ${num1 / num2}`);
+        break;
+        default:
+            console.log();
+            break;
+
+        
+
+    }
+} /*end of calculation*/
+
+calculator(5, 5, '+')
+calculator(5, 5, '-')
+calculator(5, 5, '*')
+calculator(5, 5, '/')
+
+
