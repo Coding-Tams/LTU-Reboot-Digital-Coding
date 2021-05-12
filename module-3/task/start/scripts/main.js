7533e82f6fbbfe83bf40bddfe0e0c771611421b4
Coding-Tams <82816924+Coding-Tams@users.noreply.github.com>
2021-05-12 09:24:18 +0100

    Update main.js
    I  commented out the work from the previous lessons that I didn't need.
    used the shopping basket script to  make a shopping cart.

--- module-3/task/start/scripts/main.js
+++ module-3/task/start/scripts/main.js
@@ -4,7 +4,7 @@ const productName = 'Baked beans';
 let price = 0.89;
 let quantity = 2;
 let inStock = true;
-let discountAmount = 0.20;
+let discountAmount = 0.20; 
 
 //console.log( productName, price );
 
@@ -81,13 +81,13 @@ function calculator (num1, num2, operator) {
 
         
 
-    }
+    } 
 } /*end of calculation*/
 
-/*calculator(5, 5, '+')
+calculator(5, 5, '+')
 calculator(5, 5, '-')
 calculator(5, 5, '*')
-calculator(5, 5, '/')*/
+calculator(5, 5, '/')
 
 /*let prouductArray = [productName, price, quantity, inStock, discountAmount]
 
@@ -108,10 +108,10 @@ let productAsAnItem ={
     discountApplied: 0.2,
 };
 
-//console.log(productAsAnItem.productName)
+console.log(productAsAnItem.productName);
 
 
-//productAsAnObject.price =1.25 
+//productAsAnObject.price =1.25; 
 
 //productAsAnObject.instock = false;
 
@@ -121,22 +121,60 @@ let productAsAnItem ={
  
 //delete.productAsAnObject
 
-/*let numbers =[1,2,3,4,5,6,7,8,9,];
+let numbers =[1,2,3,4,5,6,7,8,9,];
 for( let counter = 0; counter < 5; counter++){
     console.log(counter);
 
-}*/
+} 
 
-/* for loop
+ /*for loop
 counter = 1
 counter <= 12 
 counter++*/
 
-for(let counter = 1; counter <= 12; counter++ ){
+/*for(let counter = 1; counter <= 12; counter++ ){
     let result = num * counter;
     console.log( '${num} * ${counter} = ${result}' );
 
 }
-multiplyBy(10);
+multiplyBy(10);*/
 
+function totalPriceOfShopping(arr) {
+    let totalPrice = 0;
+    for(let counter = 0; arr.length; counter++); {
+       totalPrice = totalPrice + arr[counter].price * arr[counter].quantity;
+
+    }
+    return totalPrice.toFixed(2);
+
+}
+
+console.log(totalPriceOfShopping(shoppingCart));
+
+function discountToiletries(arr) {
+    let totalPrice = 0;
+    for( let counter = 0; arr.length; counter++); {
+        if(type === 'toiletries') {
+            totalPrice = (toiletries / 50) + price;
+        }
+        else {
+            price = totalPrice +
+        }
+    return totalPriceOfShopping;
+
+    }
+      
+}
+console.log(discountToiletries (shoppingCart));
+
+
+
+
+
+
+5. If it is then remove 50% from its price and add it to the totalPrice
+6. Else just work out the price and add it to the totalPrice
+7. outside of the for loop but still inside the function return totalPrice
+8. 
+   * Your output should be: 68.22
 
